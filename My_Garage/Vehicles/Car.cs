426bb9d3f0e75366998056a27a5136af723a9ce4
Martin Sisk <Brothermj@hotmail.com>
2021-08-07 12:18:05 -0500

    Added Console.WriteLine string if  conditions to Refueling, Driving, and Braking Methods in the Car Class.

--- My_Garage/Vehicles/Car.cs
+++ My_Garage/Vehicles/Car.cs
@@ -10,26 +10,42 @@ namespace My_Garage.Vehicles
     {
         // Public Properties
         public short Fuel { get; set;} 
+        public short Battery { get; set; }
         public string Color { get; set; }
-
         public int Occupancy { get; set; }
 
         // Refueling Method
         public void Refueling()
         {
+            if (Fuel <= 33)
+            {
+                Console.WriteLine("Stop at the nearest gas station ahead.");
+            }
+            else if (Battery <= 20)
+            {
+                Console.WriteLine("Find the nearest Tesla charging station");
+            }
 
         }
 
         // Driving Method
         public void Driving()
         {
+            if (Occupancy == 1)
+            {
+                Console.WriteLine("Go ten miles above the speed limit.");
+            }
+            else if (Occupancy >= 2)
+            {
+                Console.WriteLine("Drive safe and go the speed limit.");
+            }
 
         }
 
         //  Braking Method
         public void Braking()
         {
-
+            Console.WriteLine("Pump the breaks.");
         }
 
 
