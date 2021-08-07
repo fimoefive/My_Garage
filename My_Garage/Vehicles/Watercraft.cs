2b0ac0f13bc76b43f7f60115f802cdecd86646db
Martin Sisk <Brothermj@hotmail.com>
2021-08-07 12:42:34 -0500

    Added Console string statements to Refueling and Driving Methods in Watercraft.cs Class file. Also added a public bool property for Fishing.

--- My_Garage/Vehicles/Watercraft.cs
+++ My_Garage/Vehicles/Watercraft.cs
@@ -12,19 +12,29 @@ namespace My_Garage.Vehicles
         public short Fuel { get; set; }
         public string Color { get; set; }
         public int Occupancy { get; set; }
+        public bool Fishing { get; set; }
 
         // Method Refueling
         public void Refueling()
         {
-
+    if (Fuel <= 33)
+            {
+                Console.WriteLine("Head back to the dock to refuel.");
+            }
         }
 
         // Method Driving
         public void Driving()
         {
-
+            if (Fishing == true)
+            {
+                Console.WriteLine("Use the trolly motor.");
+            }
+            else if (Fishing == false)
+            {
+                Console.WriteLine("Run the outboard motor.");
+            }
         }
-
 
 
     }
